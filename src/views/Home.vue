c75fc4ac2a42a1821a5ce2999c692be9cc4d0034
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2023-06-20 22:27:16 -0500

    Included vuex and router for letters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,31 @@
 <template>
-    Home
+    <div class="flex flex-col p-8">
+
+        <div>
+            <input
+            type="text"
+            class="rounded border-2 border-gray-200 w-full"
+            placeholder="Search for meals"/>
+        </div>
+        
+        <div class="flex gap-1 justify-center gap-2 mt-2">
+            <router-link 
+                v-for="(letter,index) in letters"
+                :key="index"
+                :to="{name: 'byLetter', params: {letter}}" >
+                {{ letter }}
+            </router-link>
+        </div>
+    </div>
+    
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import store from '../store'
+
+const meals = computed(() => store.state.meals);
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
 
 </script>
 
